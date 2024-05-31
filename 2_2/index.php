cb050d9b3a0708937fb5f49a7b1712be83fdf473
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-05-31 11:07:00 +0300

    Initial commit

---
+++ 2_2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATE APP (Task 2.2)</title>
+</head>
+<body>
+    <h1>DATE APP (TASK 2.2)</h1>
+
+    <table border="1" cellspacing="0" cellpadding="10">
+        <tr>
+            <td>Current Date:</td>
+            <td>CURRENT</td>
+        </tr>
+        <tr>
+            <td>Add:</td>
+            <td>
+                <input type="number"> day(s)
+            </td>
+        </tr>
+        <tr>
+            <td>Result:</td>
+            <td>RESULT</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit">
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
