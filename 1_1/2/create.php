305edfdab94e0cb3ec6d3a75332bf186eab56886
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-05-31 13:06:41 +0300

    Update

---
+++ 1_1/2/create.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diary App (Task 1.1.2)</title>
+    <style>
+        label {
+            width: 125px;
+            display: inline-block;
+        }
+    </style>
+</head>
+<body>
+    <h1>DIARY APP (TASK 1.1.2)</h1>
+
+    <form action="?" method="post">
+        <label for="num">Entry Number:</label>
+        <input type="number" name="num" id="num">
+        <br>
+
+        <label for="content">Content:</label>
+        <textarea name="content" id="content" cols="60" rows="10"></textarea>
+        <br>
+
+        <label for="">&nbsp;</label>
+        <button>Save</button>
+    </form>
+</body>
+</html>
