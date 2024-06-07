a83cb5102b45d6d124570aae6ed4e59d694d77e1
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-07 12:47:47 +0300

    Modern

---
+++ 2_3/modern/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students</title>
+</head>
+<body>
+    <h1>STUDENTS</h1>
+
+    <?php
+    $DBCONN = new mysqli('localhost', 'root', '', 'icsa_webprogramming_university_db');
+    $result = $DBCONN->query("SELECT * FROM student ORDER BY id");
+    ?>
+
+    <table border="1" cellspacing="0" cellpadding="10" width="66%" align="center">
+        <tr>
+            <th width="75">ID</th>
+            <th>NAME</th>
+        </tr>
+        <?php
+        while ($row = $result->fetch_assoc()) { ?>
+            <tr>
+                <td>
+                    <a href="javascript:loadData('<?= $row['id'] ?>')">
+                        <?= $row['id'] ?>
+                    </a>
+                </td>
+                <td>
+                    <?php
+                    if (
+                        isset($_GET['id'])
+                        && $_GET['id'] == $row['id']
+                    ) {
+                        echo $row['name'];
+                    } ?>
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+    <script>
+        async function loadData(n) {
+            const response = await fetch('loaddata.php');
+            console.log(response.text());
+        }
+    </script>
+</body>
+</html>
