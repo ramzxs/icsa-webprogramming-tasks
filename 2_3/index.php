8f79595113dad52813e0b5311f034518fab46fdb
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-07 11:47:15 +0300

    Basic App

--- 2_3/index.php
+++ 2_3/index.php
@@ -10,38 +10,33 @@
 
     <?php
     $DBCONN = new mysqli('localhost', 'root', '', 'icsa_webprogramming_university_db');
-    $result = $DBCONN->query("SELECT * FROM `student` ORDER BY `id`");
+    $result = $DBCONN->query("SELECT * FROM student ORDER BY id");
     ?>
 
-    <table border="1" cellspacing="0" cellpadding="10">
+    <table border="1" cellspacing="0" cellpadding="10" width="66%" align="center">
         <tr>
-            <td>ID</td>
-            <td>NAME</td>
+            <th width="75">ID</th>
+            <th>NAME</th>
         </tr>
-        <tr>
-            <td>
-                <a href="index.php?id=123">123</a>
-            </td>
-            <td>
-                <?php
-                if ( isset($_GET['id']) && $_GET['id'] == '123') {
-                    echo 'Alpha';
-                }
-                ?>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <a href="index.php?id=124">124</a>
-            </td>
-            <td>
-                <?php
-                if ( isset($_GET['id']) && $_GET['id'] == '124') {
-                    echo 'Bravo';
-                }
-                ?>
-            </td>
-        </tr>
+        <?php
+        while ($row = $result->fetch_assoc()) {
+            // print_r($row); ?>
+            <tr>
+                <td>
+                    <a href="?id=<?= $row['id'] ?>">
+                        <?= $row['id'] ?>
+                    </a>
+                </td>
+                <td>
+                    <?php
+                    if ( isset($_GET['id']) && $_GET['id'] == $row['id']) {
+                        echo $row['name'];
+                    } ?>
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
     </table>
 </body>
 </html>
