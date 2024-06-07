3a91656b54329cb5172b948e67f2b00bded1be5b
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-07 12:53:18 +0300

    Query

--- 2_3/modern/loaddata.php
+++ 2_3/modern/loaddata.php
@@ -1,3 +1,8 @@
 <?php
-echo "SAMPLE_DATA";
+$DBCONN = new mysqli('localhost', 'root', '', 'icsa_webprogramming_university_db');
+$result = $DBCONN->query("SELECT * FROM student WHERE id = '24-0001' ");
+
+$row = $result->fetch_assoc();
+
+echo $row['name'];
 ?>
