6de37e68d08c7389fe17c0810276a7d221773d43
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-07 13:40:15 +0300

    Customize

--- 2_3/modern/index.php
+++ 2_3/modern/index.php
@@ -4,8 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students (Modern)</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: monospace;
+            color: white;
+            background-color: black;
+        }
+
+        a {
+            text-decoration: none;
+            color: yellow;
+            background-color: #666;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            margin: 4px;
+            display: inline-block;
+        }
+
+        table {
+            border: 2px solid white;
+        }
+        thead th {
+            color: #ccc;
+            background-color: #666;
+        }
+    </style>
 </head>
 <body>
+    <img src="header.png" alt="University" width="100%">
+
     <h1>STUDENTS (MODERN)</h1>
 
     <?php
@@ -13,30 +42,34 @@
     $result = $DBCONN->query("SELECT * FROM student ORDER BY id");
     ?>
 
-    <table border="1" cellspacing="0" cellpadding="10" width="66%" align="center">
-        <tr>
-            <th width="75">ID</th>
-            <th>NAME</th>
-        </tr>
-        <?php
-        for ($i = 1; $row = $result->fetch_assoc(); $i++) { ?>
+    <table cellspacing="0" cellpadding="10" width="66%" align="center">
+        <thead>
             <tr>
-                <td>
-                    <a href="javascript:loadData(
-                            '<?= $row['id'] ?>',
-                            '<?= $i ?>'
-                    )">
-                        <?= $row['id'] ?>
-                    </a>
-                </td>
-                <td id="cell<?= $i ?>">
-                    <!-- AJAX -->
-                </td>
+                <th width="85">ID</th>
+                <th>NAME</th>
             </tr>
-            <?php         
-        }
-        ?>
-    </table>
+        </thead>
+        <tbody>
+            <?php
+            for ($i = 1; $row = $result->fetch_assoc(); $i++) { ?>
+                <tr>
+                    <td>
+                        <a href="javascript:loadData(
+                                '<?= $row['id'] ?>',
+                                '<?= $i ?>'
+                        )">
+                            <?= $row['id'] ?>
+                        </a>
+                    </td>
+                    <td id="cell<?= $i ?>">
+                        <!-- AJAX -->
+                    </td>
+                </tr>
+                <?php         
+            }
+            ?>
+        </tbody>
+    </table>    
     <script>
         async function loadData(studentID, cellNum) {
             const response = await fetch('loaddata.php?id=' + studentID);
