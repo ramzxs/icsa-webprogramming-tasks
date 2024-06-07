111c82ca2dc39fc7c8a60a3e883fd9c5ec772731
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-07 12:19:20 +0300

    Code Format

--- 2_3/index.php
+++ 2_3/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students</title>
 </head>
+
 <body>
     <h1>LIST OF STUDENTS</h1>
 
@@ -20,7 +22,8 @@
         </tr>
         <?php
         while ($row = $result->fetch_assoc()) {
-            // print_r($row); ?>
+            // print_r($row); 
+        ?>
             <tr>
                 <td>
                     <a href="?id=<?= $row['id'] ?>">
@@ -29,14 +32,18 @@
                 </td>
                 <td>
                     <?php
-                    if ( isset($_GET['id']) && $_GET['id'] == $row['id']) {
+                    if (
+                        isset($_GET['id'])
+                        && $_GET['id'] == $row['id']
+                    ) {
                         echo $row['name'];
                     } ?>
                 </td>
             </tr>
-            <?php
+        <?php
         }
         ?>
     </table>
 </body>
+
 </html>
--- 2_3/post.php
+++ 2_3/post.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students</title>
 </head>
+
 <body>
     <h1>LIST OF STUDENTS</h1>
 
@@ -15,29 +17,33 @@
 
     <form action="?" method="post">
         <input type="hidden" name="SECRET" value="A1B2C3">
-    <table border="1" cellspacing="0" cellpadding="10" width="66%" align="center">
-        <tr>
-            <th width="75">ID</th>
-            <th>NAME</th>
-        </tr>
-        <?php
-        while ($row = $result->fetch_assoc()) {
-            // print_r($row); ?>
+        <table border="1" cellspacing="0" cellpadding="10" width="66%" align="center">
             <tr>
-                <td>
-                    <button name="id" value="<?= $row['id'] ?>"><?= $row['id'] ?></a>
-                </td>
-                <td>
-                    <?php
-                    if ( isset($_POST['id']) && $_POST['id'] == $row['id']) {
-                        echo $row['name'];
-                    } ?>
-                </td>
+                <th width="75">ID</th>
+                <th>NAME</th>
             </tr>
             <?php
-        }
-        ?>
-    </table>
+            while ($row = $result->fetch_assoc()) {
+                // print_r($row); 
+            ?>
+                <tr>
+                    <td>
+                        <button name="id" value="<?= $row['id'] ?>">
+                            <?= $row['id'] ?>
+                        </button>
+                    </td>
+                    <td>
+                        <?php
+                        if (isset($_POST['id']) && $_POST['id'] == $row['id']) {
+                            echo $row['name'];
+                        } ?>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
     </form>
 </body>
+
 </html>
