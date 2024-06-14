84ea428b865797dc1f8730a0d0812fffd84983de
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-14 14:04:55 +0300

    Dashboard

--- 1_4/libs/main.php
+++ 1_4/libs/main.php
@@ -21,7 +21,7 @@
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Q8University</a>
+                <a class="navbar-brand" href=".">Q8University</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -30,7 +30,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="."><?= ($userIsIn == 'Y') ? 'Dashboard' : 'Home' ?></a>
+                            <a class="nav-link active" aria-current="page" href="<?= ($userIsIn == 'Y') ? 'dashboard.php' : '.' ?>">
+                                <?= ($userIsIn == 'Y') ? 'Dashboard' : 'Home' ?>
+                            </a>
                         </li> <?php
                         if ($userIsIn == 'N') { ?>
                             <li class="nav-item">
