8f54b5396282861bb3b53d95bf0f6e6b898fce1c
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-06-14 12:51:05 +0300

    About Page

---
+++ 1_3/about.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'libs/main.php';
+
+printHeader();
+?>
+
+<main class="container">
+    <h1>About</h1>
+
+    <p>Q8 University is.... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis facere corrupti deleniti impedit quis necessitatibus dolores cum autem aut, possimus sunt quisquam repudiandae cupiditate non qui nulla. Voluptas, totam laboriosam!</p>
+
+    <p>This eLearning Platform is.... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat deserunt totam, asperiores, deleniti natus est, ex labore exercitationem aliquam nostrum tempora explicabo quisquam temporibus veritatis sequi et neque eius aut!</p>
+</main>
+
+<?php
+printFooter();
+?>
